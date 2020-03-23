bcc00a0bdf31d1f93f97d1bf7bd94f2c697553fd
theChrisElson <32608072+thechriselson@users.noreply.github.com>
2020-03-23 17:01:28 +0000

    Update residences.js

--- residences.js
+++ residences.js
@@ -48,7 +48,7 @@ var lstMaxH = "225rem";
 function changeFloor(i) {
 	let x = actvFltrs[1];
 	let y = 0; // Default (FL1)
-	if(i !== undefined) {x = i}
+	if(i !== undefined) {x = i} // Passed specific floor
 	if(x == undefined || x.length == 0) {}
 	else {y = x - 1}
 	let z = 0; // Timer delay
